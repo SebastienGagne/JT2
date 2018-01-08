b413a2dd2db5730e61b76cd2a368c2dcb83df4a2
SebastienGagne <sebastien.gagne41@orange.fr>
2018-01-08 07:56:05 +0100

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-//$_SESSION['login'] = 'seb';
 require_once ('./lib/File.php');
 require(File::build_path(array('lib','Util.php')));
 require(File::build_path(array('lib','Security.php')));
